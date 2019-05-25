5ea4f45a8c3efe5e230cbb087869de6b97e6389b
TimMaa <tim.maasen@ymail.com>
2019-05-26 00:04:15 +0200

    Add basic Transitions

---
+++ src/transitions/FlyOutTransition.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Transition } from 'react-transition-group';
+
+export default function FlyOutTransition({ children, direction, in: inProp, timeout }) {
+  const defaultStyle = {
+    transition: `margin-top ${timeout}ms ease-in-out`,
+    marginTop: '-100vh',
+  };
+
+  const transitionStyles = {
+    entering: { marginTop: '-100vh' },
+    entered: { marginTop: 0 },
+    exiting: { marginTop: 0 },
+    exited: { marginTop: '-100vh' }
+  };
+
+  return (
+    <Transition in={inProp} timeout={0} classNames={`fly-${direction}`}>
+      {state => (
+        <div style={{
+          ...defaultStyle,
+          ...transitionStyles[state]
+        }}>
+          {children}
+        </div>
+      )}
+    </Transition>
+  );
+}
+
+FlyOutTransition.propTypes = {
+  direction: PropTypes.string.isRequired,
+  in: PropTypes.bool.isRequired,
+  timeout: PropTypes.number,
+};
+
+FlyOutTransition.defaultProps = {
+  timeout: 300,
+};
