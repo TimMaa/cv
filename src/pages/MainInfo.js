11b0e8531f52ed2a25bf9389b5e1c17bad350851
TimMaa <tim.maasen@ymail.com>
2019-05-26 15:27:02 +0200

    Add category fly animation

--- src/pages/MainInfo.js
+++ src/pages/MainInfo.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
 
 import TextCard from '../components/TextCard';
 import Personal from '../components/Personal';
@@ -7,12 +8,39 @@ import Education from '../components/Education';
 import Other from '../components/Other';
 
 function MainInfo() {
+
+  const [ active, setActive ] = useState(false);
+
+  const duration = 300;
+  
+  const defaultStyle = (dir1, dir2) => ({
+    transition: `${dir1} ${duration}ms ease-in-out,${dir2} ${duration}ms ease-in-out`,
+    [dir1]: '-200px',
+    [dir2]: '-200px'
+  });
+  
+  const transitionStyles = (dir1, dir2) => ({
+    entering: { [dir1]: '0px', [dir2]: '0px' },
+    entered: { [dir1]: '-200px', [dir2]: '-200px' },
+    exiting: { [dir1]: '-200px', [dir2]: '-200px' },
+    exited: { [dir1]: '0px', [dir2]: '0px' },
+  });
+
+  console.log(transitionStyles('top', 'left'));
+
   return (
     <div className="main-info">
       <div className="categories">
-        <div className="personal">
-          <Personal />
-        </div>
+        <Transition in={active} timeout={duration}>
+          {state => (
+            <div className="personal" style={{
+              ...defaultStyle('top', 'left'),
+              ...transitionStyles('top', 'left')[state]
+            }}>
+              <Personal />
+            </div>
+          )}
+        </Transition>
         <div className="professional">
           <Professional />
         </div>
@@ -23,7 +51,7 @@ function MainInfo() {
           <Other />
         </div>
       </div>
-      <div>
+      <div onMouseOver={() => setActive(true)} onMouseLeave={() => setActive(false)}>
         <TextCard text="Tim Maaßen" />
       </div>
       <div className="copyright">by Tim Maaßen - 2019</div>
